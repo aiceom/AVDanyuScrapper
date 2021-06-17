b79565999fc72f431df77edcdfcdbbb472be7fe6
Bingnan Li <leebingnam@hotmail.com>
2021-06-16 22:04:22 -0400

    Initial commit

---
+++ AvdanyuScraper/Program.cs
@@ -0,0 +1,68 @@
+﻿using AvdanyuScraper.Services;
+using System;
+using System.IO;
+using System.Linq;
+using Serilog;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AvdanyuScraper
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            ConfigureLogging();
+            var movieInfoSrv = new MovieInformationService();
+            var nfoSrv = new NfoService();
+            var fbd = new FolderBrowserDialog();
+
+            while (fbd.ShowDialog() == DialogResult.OK)
+            {
+                var folders = Directory.GetDirectories(fbd.SelectedPath);
+                Log.Debug($"已选定 {fbd.SelectedPath} ，开始获取元数据...");
+                Task[] taskArray = new Task[1];
+                var runningTasks = 0;
+                for(int i = 0; i < folders.Length; ++i)
+                {
+                    var folder = folders[i];
+                    if (runningTasks < taskArray.Length)
+                    {
+                        taskArray[runningTasks] = Task.Factory.StartNew(() =>
+                        {
+                            var searchName = folder.Split("\\").Last();
+                            if (searchName != ".actors")
+                            {
+                                Log.Debug($"Thread {Thread.CurrentThread.ManagedThreadId}: 开始获取 {searchName} 元数据...");
+                                var movieInfomation = movieInfoSrv.GetMovieInformation(searchName);
+                                nfoSrv.UpdateNfoData(movieInfomation, folder);
+                                Log.Debug($"Thread {Thread.CurrentThread.ManagedThreadId}: 完成获取 {searchName} 元数据！\n\r");
+                            }
+                        });
+                        runningTasks++;
+                    }
+                    if (runningTasks == taskArray.Length)
+                    {
+                        Task.WaitAll(taskArray);
+                        runningTasks = 0;
+                    }
+                }
+                Task.WaitAll(taskArray);
+                Log.Debug($"{fbd.SelectedPath} 的元数据已全部获取！");
+            }
+            Console.WriteLine("按任意键退出...");
+            Console.ReadKey();
+        }
+
+        private static void ConfigureLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File($"logs/logs-{DateTime.Now.ToString("yyyyMMddHHmmss")}.txt")
+                .CreateLogger();
+        }
+    }
+}
---
+++ AvdanyuScraper/Services/NfoService.cs
@@ -0,0 +1,51 @@
+﻿using AvdanyuScraper.ClassLibrary;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Xml;
+
+namespace AvdanyuScraper.Services
+{
+    public class NfoService
+    {
+        public void UpdateNfoData(List<MovieInformation> movieInformation, string path)
+        {
+            var doc = new XmlDocument();
+            var allNfos = Directory.GetFiles(path, "*.nfo");
+            foreach (var filename in allNfos)
+            {
+                try
+                {
+                    MovieInformation movieInfo = movieInformation.Where(x => filename.Contains(x.Code))?.FirstOrDefault();
+                    if (movieInfo != null)
+                    {
+                        Log.Debug($"Thread {Thread.CurrentThread.ManagedThreadId}: 开始添加 {movieInfo.Code} {movieInfo.Title} 元数据...");
+                        doc.Load(filename);
+                        XmlElement parent = (XmlElement)doc.DocumentElement.SelectSingleNode("/movie");
+                        foreach (var danyu in movieInfo.Danyus)
+                        {
+                            XmlElement danyuElement = doc.CreateElement("actor");
+                            danyuElement.InnerXml = $"<name>{danyu}</name><type>Actor</type>";
+                            parent.AppendChild(danyuElement);
+                        }
+                        doc.Save(filename);
+                        Log.Debug($"Thread {Thread.CurrentThread.ManagedThreadId}: {movieInfo.Code} {movieInfo.Title} 添加完毕！");
+                    }
+                    else
+                    {
+                        Log.Warning($"Thread {Thread.CurrentThread.ManagedThreadId}: 未能找到与 {filename} 匹配的元数据");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error($"Thread {Thread.CurrentThread.ManagedThreadId}: 更新 {filename} 时发生错误，错误信息: \n {ex.Message}");
+                }
+
+            }
+        }
+    }
+}
